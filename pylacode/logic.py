3077c3eed95478de348d94f0b55268a95e9a4c78
plcp <matthieu@daumas.me>
2017-07-04 17:57:32 +0200

    Add submodule stub + Add logic stubs

---
+++ pylacode/logic.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+from __future__ import unicode_literals, division, with_statement
+
+import sys
+assert sys.version_info >= (2, 7)
+
+class obsl:
+    '''Opinion-Based Subjective Logic (as found in the litterature)
+
+    '''
+    pass
+
+class tbsl:
+    '''Three-Value-Based Subjective Logic
+
+    '''
+    pass
+
+class ebsl:
+    '''Evidence-Based Subjective Logic
+
+    '''
+    pass
+
