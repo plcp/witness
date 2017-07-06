ae4cb1deda682b51a62c132c595d72db16f6e78e
plcp <matthieu@daumas.me>
2017-07-06 15:27:53 +0200

    Add logic base class

--- pylacode/logic.py
+++ pylacode/logic.py
@@ -5,21 +5,47 @@ from __future__ import unicode_literals, division, with_statement
 import sys
 assert sys.version_info >= (2, 7)
 
-class obsl:
+import warnings
+
+class _base:
+    '''Base class for logic types
+
+    '''
+
+    def __init__(self, value=None, size=None):
+        if value is not None:
+            assert value in types
+            self.size = len(value)
+
+            if size is not None:
+                warnings.warn('Size given but ignored')
+
+        else:
+            assert size is not None
+            self.size = size
+
+class obsl(_base):
     '''Opinion-Based Subjective Logic (as found in the litterature)
 
     '''
-    pass
+    def __init__(self, value=None, size=None):
+        _base.__init__(self, value, size)
+        pass
 
-class tbsl:
+class tbsl(_base):
     '''Three-Value-Based Subjective Logic
 
     '''
-    pass
+    def __init__(self, value=None, size=None):
+        _base.__init__(self, value, size)
+        pass
 
-class ebsl:
+class ebsl(_base):
     '''Evidence-Based Subjective Logic
 
     '''
-    pass
+    def __init__(self, value=None, size=None):
+        _base.__init__(self, value, size)
+        pass
 
+types = [obsl, tbsl, ebsl]
