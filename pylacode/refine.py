b20e5b23dd9f9604e64f5037a0b592dec8ca9c8c
plcp <matthieu@daumas.me>
2017-08-02 10:40:13 +0200

    Add label refining backend (*not* tested for now)

--- pylacode/refine.py
+++ pylacode/refine.py
@@ -6,7 +6,10 @@ import sys
 import pylacode as pl
 assert sys.version_info >= (2, 7)
 
+import pylacode.source
+import pylacode.fuzzy
 import warnings
+import operator
 
 class NoDataRefinedError(Exception):
     pass
@@ -30,3 +33,77 @@ class data(object):
 
     def __repr__(self):
         return self.name + object.__repr__(self)
+
+class label(data):
+    def __init__(self, name, size, source=None, **mdata):
+        self.size = size
+        self.name = name
+        self.mdata = dict(**mdata)
+        self.labels = {}
+
+        if source is None:
+            self.source = pl.source.label_source(tag=self.name)
+        else:
+            assert pl.source.issource(source)
+            self.source = source
+
+    def add(self, label, *labels):
+        if isinstance(label, tuple):
+            if len(label) >= 2:
+                assert isinstance(label[0], type(''))
+                assert pl.logic.islogic(label[1])
+
+            if len(label) == 2:
+                assert len(label[1]) == self.size
+                self.labels[label[0]] = (label[1].tbsl, slice(None));
+            elif len(label) == 3:
+                assert isinstance(label[3], slice)
+                assert len(label[1]) == len(pl.logic.tbsl(self.size)[label[3]])
+                self.labels[label[0]] = (label[1].tbsl, label[3])
+            else:
+                raise AssertionError('Ill-formed by-tuple label')
+        elif isinstance(label, type('')):
+            assert(len(self.labels) < self.size)
+
+            _value = None
+            if label.startswith('!'):
+                _value = pl.logic.tbsl(pl.logic.tbsl.false())
+            elif label.startswith('?'):
+                _value = pl.logic.tbsl(pl.logic.tbsl.uncertain())
+            else:
+                _value = pl.logic.tbsl(pl.logic.tbsl.true())
+            _slice = slice(len(self.labels))
+
+            self.labels[label] = (_value, _slice)
+        else:
+            raise AssertionError('Unable to use {} to construct a label'.format(
+                label))
+
+    def get_label(self, label):
+        if label not in self.labels:
+            return None
+
+        _evidence = pl.fuzzy.evidence(
+            size=self.size,
+            source=self.source,
+            origin='label',
+            label=label)
+        _value, _slice = self.labels[label]
+
+        _evidence.value[_slice] = _value
+        return _evidence
+
+    def transform(self, state):
+        for idx, payload in state.remaining_data:
+            _label = self.get_label(payload)
+            if _label is None and payload[0].startswith('!'):
+                _label = self.get_label(payload[1:])
+                if _label is not None:
+                    _label.value.invert()
+
+            if _label is not None:
+                state.ouput.append(_label)
+                del state.remaining_data[idx]
+
+    def inverse(self, state):
+       return # todo
