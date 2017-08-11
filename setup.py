673b43ed6a4354c8bc9a3be49e3e3c547f42553c
plcp <matthieu@daumas.me>
2017-08-11 22:53:48 +0200

    Add setup.{py|cfg} for further release

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='witness',
+    packages=['witness'],
+    version='0.0.2',
+    description=(
+        'Knowledge abstraction layer: provides automation to extract, ' +
+        'aggregate and query knowledge based on subjective logic.'),
+    author='plcp',
+    author_email='matthieu@daumas.me',
+    url='https://github.com/plcp/witness',
+    download_url="<release>", )
