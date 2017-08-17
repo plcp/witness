2e5e0669039fd3f164946e193d7838914b773149
plcp <matthieu@daumas.me>
2017-08-17 16:37:51 +0200

    Fix cold/warm semantic

--- example.py
+++ example.py
@@ -6,16 +6,20 @@ import witness.boil
 
 
 def build_weather_labels():
-    w = wit.refine.label(name='weather', size=5)
-    w.add(label='cold')
+    w = wit.refine.label(name='weather', size=6)
+    w.add(label='warm', value=[True, False])
+    w.add(label='cold', value=[False, True], where=slice(0, 2))
+    w.add(label='temp', where=slice(0, 2))
+
     w.add(label='rainy')
     w.add(label='cloudly')
     w.add(label='windy')
+
     w.add(label='!thunder', value=False)
-    w.add(label='thunder', value=[True, None, True], where=slice(2, 5))
-    w.add(label='warm', value=False, where=slice(0, 1))
-    w.add(label='rainstorm', where=slice(0, 3))
-    w.add(label='thunderstorm', where=slice(1, 5))
+    w.add(label='thunder', value=[True, None, True], where=slice(3, 6))
+
+    w.add(label='rainstorm', where=slice(1, 4))
+    w.add(label='thunderstorm', where=slice(2, 6))
     return wit.table.translation(w)
 
 
