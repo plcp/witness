0348dcf2913c571abe92a914e23177f5dc371b94
plcp <matthieu@daumas.me>
2017-08-21 16:02:03 +0200

    Add test_modules/boil.py as helper

---
+++ witness/test_modules/boil.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+##
+# witness
+#
+#  Copyright 2017 by Matthieu Daumas <matthieu@daumas.me> and other authors.
+#
+# This file is a part of fuddly, as part of the knowledge component.
+#
+#  Licensed under GNU General Public License 3.0 or later.
+#  Some rights reserved. See COPYING, AUTHORS.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+##
+
+from test_fuzzy import *
+from test_logic import *
+from test_refine import *
+from test_source import *
+from test_table import *
+from test_weather import *
