22cb8cc0e007626fd4f9b9198923f0bef040f669
plcp <matthieu@daumas.me>
2017-08-21 15:15:28 +0200

    Add copyright notices + Clarify licensing

--- witness/backends/__init__.py
+++ witness/backends/__init__.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+
+##
+# witness
+#
+#  Copyright 2017 by Matthieu Daumas <matthieu@daumas.me> and other authors.
+#
+# This file is a part of fuddly, as part of the knowledge component.
+#
+#  Licensed under GNU General Public License 3.0 or later.
+#  Some rights reserved. See COPYING, AUTHORS.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+##
+
 import witness.backends.naive
 
 # backends enabled by default
--- witness/test_modules/test_weather.py
+++ witness/test_modules/test_weather.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 
+##
+# witness
+#
+#  Copyright 2017 by Matthieu Daumas <matthieu@daumas.me> and other authors.
+#
+# This file is a part of fuddly, as part of the knowledge component.
+#
+#  Licensed under GNU General Public License 3.0 or later.
+#  Some rights reserved. See COPYING, AUTHORS.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+##
+
 import unittest
 
 import witness as wit
